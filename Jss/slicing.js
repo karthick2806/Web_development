dd5df729989d9aaa18c68cf721091f4cb93de006
karthick2806 <karthick280603@gmail.com>
2025-07-16 09:58:38 -0700

    updated css

--- Jss/slicing.js
+++ Jss/slicing.js
@@ -1,4 +1,4 @@
-var c =prompt("write to your character");
+var c = prompt("write to your character");
 var b = c.slice(0,140);
 alert("you've written"+" "+(c.length)+" "+"you've reached the limit at "+b+" words of 140 words by typing"+" "+b.length);
 
