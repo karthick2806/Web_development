a09a08db4a91962c70dc403aa246cc8be35bd1c5
Karthick2806 <karthick280603@gmail.com>
2025-08-21 13:07:10 -0700

    Array sort added

---
+++ Frontend/Jss/1.7_Js_arrays/array_sort.js
@@ -0,0 +1,5 @@
+let fruits = ["Apple","Orange","Banana","Dragon fruit","Kiwi","Pear","Pome granate"]
+console.log(fruits.sort())            //make changes on original array
+console.log(fruits.reverse())
+console.log(fruits.toSorted())        //creates a copy and leaves original unchanged
+console.log(fruits.toReversed())
