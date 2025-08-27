073fd82f3896e9fd272a90ddc41794dafe3694af
Karthick2806 <karthick280603@gmail.com>
2025-08-26 23:38:39 -0700

    added explanation on callbacks

--- Frontend/Jss/2.0_Js_async/callbacks.js
+++ Frontend/Jss/2.0_Js_async/callbacks.js
@@ -9,9 +9,27 @@ function greets(name){
 }
 
 function talks(name){
-     return ("How was your day today? "+name)
+    return ("How was your day today? "+name)  
+/*
+-->if widout display (callback function) means the calllback will be here in talks() like
+callback() or a param with it like process (name) that too coz return keyword
+
+-->then the name value goes back to the arg process (the process name is user defined)
+
+-->then the name value(name) got shared by the function marked process here but called as greet() or talk() down there
+
+-->then coz of return the funct call wrapped inside console.log
+
+
+*/
    
 }
 
 console.log(display(greets,"Karthik"))    //greeets goes like process and takes 
-console.log(display(talks,"Karthik"))     
+console.log(display(talks,"Karthik"))     //if talk() and greet() built with console.log means here there is no need
+
+
+/*
+basically here this is the syntax
+*/
+
---
+++ Frontend/Jss/2.0_Js_async/setTimeout.js
@@ -0,0 +1,10 @@
+function add(num1,num2,oper){
+    let result = num1+num2
+    return result
+}
+
+let timer = (()=> console.log("setTimeout function is running"))
+
+
+console.log(add(45,78,process))
+console.log(timer)
