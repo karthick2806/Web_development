508b76157586ec6cf861f14c8e2fecfc43662c74
karthick2806 <karthick280603@gmail.com>
2025-11-15 07:51:12 -0800

    enumeration datatype created

---
+++ Backend/Java_prgrm/src/myfirstjavaprgrm/enumeration_class.java
@@ -0,0 +1,22 @@
+package myfirstjavaprgrm;
+
+//An enum (short for enumeration) is a special data type that represents a fixed set of constants — like days of the week, directions, levels, states, etc.
+
+
+
+
+
+
+
+
+
+
+public class enumeration_class {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
