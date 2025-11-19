3260dbe558b39acbf303cba8ae0cec10345f56ec
karthick2806 <karthick280603@gmail.com>
2025-11-19 16:54:19 +0530

    enumeration got completed

--- Backend/Java_prgrm/src/myfirstjavaprgrm/enumeration_class.java
+++ Backend/Java_prgrm/src/myfirstjavaprgrm/enumeration_class.java
@@ -41,6 +41,9 @@ class sample_enum_class{
 		public void show() {
 			System.out.println("The middle of the week "+this.name()); //tells the current obj or enum const can use this for both monday and wednesday
 		}
+		
+		
+		
 	}
 }
 
@@ -67,11 +70,36 @@ enum primary implements enum_interface{
 	BLUE;
 	
 	public void print() {
-		System.out.println("One of the primary color is "+this.name());
+		System.out.println("One of the primary color is "+this.toString());  //or this.name()
 	}
 	
 	
+	
+	
 }
+
+
+
+enum numbers{
+	one,
+	two,
+	three,
+	four,
+	five,
+	six,
+	se7en,    //this enum is just to explain toString 
+	eight,
+	nine,
+	ten;
+	
+	public String toString() {
+		System.out.println("This function can be overriden by all primary enum constants");
+		return "The color is "+this.name();
+	}
+	
+}
+
+
 
 
 
@@ -88,12 +116,15 @@ public class enumeration_class {
         System.out.println(d.getnumber_of_the_week());
         
         //for the method
+        System.out.println("\n");
         sample_enum_class.days w = sample_enum_class.days.wednesday;
         
         w.show();
         
         
+        
         //for abstract method
+        System.out.println("\n");
         operation op_add = operation.add;
         operation op_sub = operation.sub;
         
@@ -102,12 +133,53 @@ public class enumeration_class {
         
         
         //for interface
+        System.out.println("\n");
         primary pr = primary.RED;
         pr.print();
         
+       
+        
+
+        //for switch case
+        System.out.println("\n");
+        switch(pr) {    //we can use the enum in switch statement also
+        case RED:
+        	System.out.println("This is the color "+pr+" using Switch");
+        	break;
+        	
+        case BLUE:
+        	System.out.println("This is the color "+pr+" using Switch");
+        	break;
+        
+        case GREEN:
+        	System.out.println("This is the color "+pr+" using Switch");
+        	break;
+        }
         
         
+        //enum built in functions
+        System.out.println("\n");
+        primary sc = primary.BLUE;
+        System.out.println(sc.name());     //returns the exact enum const no change its final as same as the enum constant
         
+        System.out.println(numbers.se7en);     //we can modify,customize the enum constants as we want not the const as it is without disturbing the constants
+        
+        System.out.println(primary.GREEN.ordinal());    //returns the position number of the const starts from 0  so green = 1
+        
+        for(primary all :primary.values()) {
+        	System.out.println(all);           //issa for loop to print all enum values
+        }
+        
+        System.out.println(primary.BLUE.compareTo(primary.GREEN));  //compare the enum constants by their ordinals
+        
+        System.out.println(pr.equals(sc)); //tells both enum are equal or not
+        
+        System.out.println(pr.getDeclaringClass());   //returns the class name of the enum class
+        
+        primary extra_color = primary.valueOf("RED");  //if user gives you a string then u can turn that a string into enum constant
+        System.out.println(extra_color);
+        
+           
 	}
 
 }
