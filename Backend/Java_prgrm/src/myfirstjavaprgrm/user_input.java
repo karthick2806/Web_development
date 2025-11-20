2f61d5bb68c4e977a5e3f08ddb27e53f265e9b77
karthick2806 <karthick280603@gmail.com>
2025-11-20 20:27:40 +0530

    worked on user input basics

---
+++ Backend/Java_prgrm/src/myfirstjavaprgrm/user_input.java
@@ -0,0 +1,55 @@
+package myfirstjavaprgrm;
+
+import java.util.Scanner;
+
+public class user_input {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter your name		:");    //println for the nextline
+		String name = sc.nextLine();  //allows multiple words
+		
+		System.out.print("Education(CSE/IT/MECH)	:");  //next allows only one line
+		String dept = sc.next();
+		
+		System.out.print("Age			:");  //int allows only integer
+		int age = sc.nextInt();
+		
+		System.out.print("CGPA			:");  //allows only double
+		double cgpa = sc.nextDouble();
+		
+		System.out.print("Percentage		:");  //allows only float
+		float percentage = sc.nextFloat();
+		
+		System.out.print("No.of.fail    		:");    //allows large numbers as input
+		long longnum = sc.nextLong();
+		
+		System.out.print("Single			:");
+		boolean rel = sc.nextBoolean();      //allows only boolean
+		
+		
+		/*
+		After using nextInt(), nextDouble(), nextFloat(), nextLong(),
+		ALWAYS write one extra nextLine() BEFORE your real nextLine().
+
+
+		
+		int n = sc.nextInt(); 
+		sc.nextLine();          // dummy read to clear ENTER
+
+		String name = sc.nextLine();  // real input
+
+		
+		If you switch from number input → String input,
+        use one extra nextLine().
+		
+		
+		*/
+		
+		
+		
+	}
+
+}
